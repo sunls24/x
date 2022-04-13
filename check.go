dd2f852b28c3761b42586065ae16b7668776c2c8
sunls <liuss.zy@outlook.com>
2022-04-13 20:24:17 +0800

    add check

--- check.go
+++ check.go
@@ -5,3 +5,12 @@ func CheckPanic(err error) {
 		panic(err)
 	}
 }
+
+func CheckEmpty(list ...string) bool {
+	for i := range list {
+		if len(list[i]) == 0 {
+			return false
+		}
+	}
+	return true
+}
